a8c463e6e5c58bb253449b07121b243c81f32e40
Suvorov-Alexandr <85122241+Suvorov-Alexandr@users.noreply.github.com>
2022-02-01 12:33:46 +0200

    hooksUpdate

--- src/App.js
+++ src/App.js
@@ -1,67 +1,80 @@
 import Container from "./AppContainer.styled.jsx";
 import GlobalStyle from "./components/GlobalStyle";
-import { Component } from "react";
+import { useState } from "react";
 import FeedbackOptions from "./components/FeedbackOptions";
 import Notification from "./components/Notification";
 import Section from "./components/Section";
 import Statistics from "./components/Statistics";
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const optionsState = { good, neutral, bad };
 
-  getStateKeys = () => Object.keys(this.state);
-  getStateValues = () => Object.values(this.state);
+  const getStateKeys = () => Object.keys(optionsState);
+  const getStateValues = () => Object.values(optionsState);
 
-  onLeaveFeedback = (evt) => {
+  const onLeaveFeedback = (evt) => {
     const currentSelection = evt.target.textContent;
-    this.setState((prevState) => {
-      return { [currentSelection]: prevState[currentSelection] + 1 };
-    });
+
+    switch (currentSelection) {
+      case "good":
+        setGood((prevState) => prevState + 1);
+        break;
+      case "neutral":
+        setNeutral((prevState) => prevState + 1);
+        break;
+      case "bad":
+        setBad((prevState) => prevState + 1);
+        break;
+      default:
+        break;
+    }
+
+    // if (currentSelection === "good") {
+    //   setGood((prevState) => prevState + 1);
+    // } else if (currentSelection === "neutral") {
+    //   setNeutral((prevState) => prevState + 1);
+    // } else {
+    //   setBad((prevState) => prevState + 1);
+    // }
   };
 
-  countTotalFeedback() {
-    return this.getStateValues().reduce((acc, state) => acc + state, 0);
-  }
+  const countTotalFeedback = () =>
+    getStateValues().reduce((acc, state) => acc + state, 0);
 
-  countPositiveFeedbackPercentage() {
-    return Math.round((this.state.good * 100) / this.countTotalFeedback());
-  }
+  const countPositiveFeedbackPercentage = () =>
+    Math.round((good * 100) / countTotalFeedback());
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = this.getStateKeys();
-    const total = this.countTotalFeedback();
-    const percentage = this.countPositiveFeedbackPercentage();
+  const optionsKeys = getStateKeys();
+  const total = countTotalFeedback();
+  const percentage = countPositiveFeedbackPercentage();
 
-    return (
-      <Container>
-        <GlobalStyle />
-        <Section title="Please leave feedback!">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.onLeaveFeedback}
+  return (
+    <Container>
+      <GlobalStyle />
+      <Section title="Please leave feedback!">
+        <FeedbackOptions
+          options={optionsKeys}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistic">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={percentage}
           />
-        </Section>
-        <Section title="Statistic">
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={percentage}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </Container>
-    );
-  }
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </Container>
+  );
 }
 
 export default App;
